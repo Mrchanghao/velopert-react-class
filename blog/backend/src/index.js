3b134c07371e9ebd0a9eaabb85f29b3f74541261
이창호 <lnl2048@gmail.com>
2018-09-04 09:56:03 +0900

    코드 스플릿, 백엔드 강좌

--- blog/backend/src/index.js
+++ blog/backend/src/index.js
@@ -1,10 +1,29 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');   
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const {
+    PORT: port = 4000,
+    MONGO_URI: mongoURI
+} = process.env;
+
+// db 연결
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI)
+.then(() => {
+    console.log('connect to mongodb');
+})
+.catch(err => {
+    console.error(err);
+});
 
 const app = new Koa();
 const router = new Router();
 const api = require('./api');
+
 
 // 라우터 설정
 router.get('/', (ctx) => {
@@ -34,6 +53,6 @@ app.use(bodyParser());
 // app 에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000, () => {
-    console.log('Koa server is running on 3000')
+app.listen(port, () => {
+    console.log('Koa server is running on ', port)
 })
